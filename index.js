567cce77624eb3e1bf3beb3b05746ef90a93a5f2
Daffa <daffaraihan03@gmail.com>
2022-07-07 09:52:19 +0700

    pertemuan 4

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-const fs = require('fs');
+// 
+// FILE SYSTEM
+// 
+const fs = require('fs')
 
 // console.log(fs);
 
-fs.writeFileSync('test.txt', 'halo dunia')
+// fs.writeFileSync('test.txt', 'halo dunia')
+
+// fs.readFile('test.txt', 'utf-8', (err, data) => {
+//     if (err) throw err
+//     console.log(data)
+// })
+
+
+//
+// RAED LINE
+// 
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question(`what's your name? `, (name) => {
+    rl.question(`what's your email? `, (email) => {
+        rl.question(`type your phone number... `, (phone) => {
+            console.log(
+                `Name : ${name} || E-mail : ${email} || Phone : ${phone}`
+            );
+            rl.close()
+        })
+    })
+})
+
+// console.log(name);
